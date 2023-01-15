53dba53c7d9d5fbcd53de68ecb0b571a4a5219b4
dangtheanh99 <theanhcuacua@gmail.com>
2023-01-15 23:51:06 +0700

    video 64

---
+++ src/controllers/doctorController.js
@@ -0,0 +1,22 @@
+import doctorService from "../services/doctorService";
+
+let getTopDoctorHome = async (req, res) => {
+  let limit = req.query.limit;
+  if (!limit) {
+    limit = 10;
+  }
+  try {
+    let doctors = await doctorService.getTopDoctor(+limit);
+    return res.status(200).json(doctors);
+  } catch (e) {
+    console.log(e);
+    return res.status(200).json({
+      errCode: -1,
+      message: "Error from server",
+    });
+  }
+};
+
+module.exports = {
+  getTopDoctorHome,
+};
---
+++ src/services/doctorService.js
@@ -0,0 +1,40 @@
+import db from "../models/index";
+
+let getTopDoctor = (limitInput) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let users = await db.User.findAll({
+        limit: limitInput,
+        where: { roleId: "R2" },
+        order: [["createdAt", "DESC"]],
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: db.Allcode,
+            as: "positionData",
+            attributes: ["valueVi", "valueEn"],
+          },
+          {
+            model: db.Allcode,
+            as: "genderData",
+            attributes: ["valueVi", "valueEn"],
+          },
+        ],
+        raw: true,
+        nest: true,
+      });
+      resolve({
+        errCode: 0,
+        data: users,
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  getTopDoctor,
+};
